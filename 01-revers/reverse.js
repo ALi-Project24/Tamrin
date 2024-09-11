ab1df93e4849fb2801ad98d5382f84ae98a09dc8
ALi24 <barname.nevis24@gmail.com>
2024-09-11 21:20:01 +0330

    Completed exercise 1

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,7 @@
-// write your func here
+function reverseString(str) {
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
+}
+console.log(reverseString("this is a string"));
--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,8 @@
-// write your func here
+function isPalindrome(str) {
+  let cleanedStr = str.replace(/\s+/g, "").toLowerCase();
+  let reversedStr = cleanedStr.split("").reverse().join("");
+  return cleanedStr === reversedStr;
+}
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("headphone"));

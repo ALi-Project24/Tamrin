278a8afb009cfd699cdb0bbf4fc75f6bbde825c4
ALi24 <barname.nevis24@gmail.com>
2024-09-11 21:29:24 +0330

    Completed exercise 3

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,14 @@
-// write your func here
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+console.log(factorial(12));
+console.log(factorial(3));

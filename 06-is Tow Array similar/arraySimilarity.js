19c0fbb62d93961df24d9350bba4b0cd6b220878
ALi24 <barname.nevis24@gmail.com>
2024-09-11 21:54:31 +0330

    Completed exercise 6

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,19 @@
-// write your func here
+function arraysEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+const input1A = [12, "1", 8, 20, 5];
+const input1B = [12, 1, 8, 20, 5];
+console.log(arraysEqual(input1A, input1B));
+
+const input2A = [12, 1, 8, 20, 5];
+const input2B = [12, 1, 8, 20, 5];
+console.log(arraysEqual(input2A, input2B));
